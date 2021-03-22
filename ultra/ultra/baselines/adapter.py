264e4210b61d3f273117b0aa32bc29a3b3a24772
AlexLewandowski <lewandowskialexp@gmail.com>
2021-03-22 00:59:50 -0600

    New branch off eval-per-episode

--- ultra/ultra/baselines/adapter.py
+++ ultra/ultra/baselines/adapter.py
@@ -195,50 +195,51 @@ class BaselineAdapter:
             ignore_vehicle_behind=True,
         )
 
-        speed_fraction = max(0, ego_observation.speed / closest_wp.speed_limit)
-        ego_step_reward = 0.02 * min(speed_fraction, 1) * np.cos(angle_error)
-        ego_speed_reward = min(
-            0, (closest_wp.speed_limit - ego_observation.speed) * 0.01
-        )  # m/s
-        ego_collision = len(ego_events.collisions) > 0
-        ego_collision_reward = -1.0 if ego_collision else 0.0
-        ego_off_road_reward = -1.0 if ego_events.off_road else 0.0
-        ego_off_route_reward = -1.0 if ego_events.off_route else 0.0
-        ego_wrong_way = -0.02 if ego_events.wrong_way else 0.0
-        ego_goal_reward = 0.0
-        ego_time_out = 0.0
-        ego_dist_center_reward = -0.002 * min(1, abs(ego_dist_center))
-        ego_angle_error_reward = -0.005 * max(0, np.cos(angle_error))
+        # speed_fraction = max(0, ego_observation.speed / closest_wp.speed_limit)
+        # ego_step_reward = 0.02 * min(speed_fraction, 1) * np.cos(angle_error)
+        # ego_speed_reward = min(
+        #     0, (closest_wp.speed_limit - ego_observation.speed) * 0.01
+        # )  # m/s
+        # ego_collision = len(ego_events.collisions) > 0
+        # ego_collision_reward = -1.0 if ego_collision else 0.0
+        # ego_off_road_reward = -1.0 if ego_events.off_road else 0.0
+        # ego_off_route_reward = -1.0 if ego_events.off_route else 0.0
+        # ego_wrong_way = -0.02 if ego_events.wrong_way else 0.0
+        ego_goal_reward = 200.0
+        # ego_time_out = 0.0
+        # ego_dist_center_reward = -0.002 * min(1, abs(ego_dist_center))
+        # ego_angle_error_reward = -0.005 * max(0, np.cos(angle_error))
         ego_reached_goal = 1.0 if ego_events.reached_goal else 0.0
-        ego_safety_reward = -0.02 if ego_num_violations > 0 else 0
-        social_safety_reward = -0.02 if social_num_violations > 0 else 0
-        ego_lat_speed = 0.0  # -0.1 * abs(long_lat_speed[1])
-        ego_linear_jerk = -0.0001 * linear_jerk
-        ego_angular_jerk = -0.0001 * angular_jerk * math.cos(angle_error)
-        env_reward /= 100
-        # DG: Different speed reward
-        ego_speed_reward = -0.1 if speed_fraction >= 1 else 0.0
-        ego_speed_reward += -0.01 if speed_fraction < 0.01 else 0.0
+        # ego_safety_reward = -0.02 if ego_num_violations > 0 else 0
+        # social_safety_reward = -0.02 if social_num_violations > 0 else 0
+        # ego_lat_speed = 0.0  # -0.1 * abs(long_lat_speed[1])
+        # ego_linear_jerk = -0.0001 * linear_jerk
+        # ego_angular_jerk = -0.0001 * angular_jerk * math.cos(angle_error)
+        # env_reward /= 100
+        # # DG: Different speed reward
+        # ego_speed_reward = -0.1 if speed_fraction >= 1 else 0.0
+        # ego_speed_reward += -0.01 if speed_fraction < 0.01 else 0.0
 
-        rewards = sum(
-            [
-                ego_goal_reward,
-                ego_collision_reward,
-                ego_off_road_reward,
-                ego_off_route_reward,
-                ego_wrong_way,
-                ego_speed_reward,
-                # ego_time_out,
-                ego_dist_center_reward,
-                ego_angle_error_reward,
-                ego_reached_goal,
-                ego_step_reward,
-                env_reward,
-                # ego_linear_jerk,
-                # ego_angular_jerk,
-                # ego_lat_speed,
-                # ego_safety_reward,
-                # social_safety_reward,
-            ]
-        )
-        return rewards
+        rewards = [ego_reached_goal, env_reward]
+        # rewards = sum(
+        #     [
+        #         ego_goal_reward,
+        #         ego_collision_reward,
+        #         ego_off_road_reward,
+        #         ego_off_route_reward,
+        #         ego_wrong_way,
+        #         ego_speed_reward,
+        #         # ego_time_out,
+        #         ego_dist_center_reward,
+        #         ego_angle_error_reward,
+        #         ego_reached_goal,
+        #         ego_step_reward,
+        #         env_reward,
+        #         # ego_linear_jerk,
+        #         # ego_angular_jerk,
+        #         # ego_lat_speed,
+        #         # ego_safety_reward,
+        #         # social_safety_reward,
+        #     ]
+        # )
+        return sum(rewards)
