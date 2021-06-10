51cb5d1b5deb1163d34808ea90aae9b112ec4e4d
Tucker <montgomery.alban@huawei.com>
2021-06-10 08:37:03 -0400

    Update copy_scenario utility

--- BM/basic/copy_scenario.py
+++ BM/basic/copy_scenario.py
@@ -10,11 +10,7 @@ dir = str(Path.absolute(Path(__file__).parent))
 def copy_to_dir(scenario_map_file, scenario_directory):
     success = False
 
-    basename = os.path.basename(scenario_map_file)
-    try:
-        os.mkdir(scenario_directory)
-    except FileExistsError:
-        pass
+    Path(scenario_directory).mkdir(parents=True, exist_ok=True)
     shutil.copyfile(
         f"{dir}/{scenario_map_file}.net.xml", f"{scenario_directory}/map.net.xml"
     )
